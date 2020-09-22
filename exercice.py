dffcdffa1be0dbe032425694093145f4c53b3ea6
yahyabadrane <yahyabadrane01@gmail.com>
2020-09-22 22:43:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -30,7 +30,7 @@ def prime_integer_summation() -> int:
 
 def is_prime_number(number):
     for i in range(2, number // 2)
-    if number % i == 0
+            if number % i == 0
             return False
 
     return True
